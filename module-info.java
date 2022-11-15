4e6fba445049738de508bc9c141d27062b6ad1f9
muzamil osman <113449526+MrrDukes@users.noreply.github.com>
2022-11-15 07:39:16 -0600

    Delete module-info.java

--- module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.futurevaluecalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.futurevaluecalculator to javafx.fxml;
-    exports com.example.futurevaluecalculator;
-}
